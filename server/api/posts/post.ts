b2688fd79e95634904f6ccb22eb7c46b8aa944c7
Jorge Mendoza-Ubuntu <mencor.corp@gmail.com>
2023-10-04 18:09:47 -0500

    Se crea un get de un archivo JSON

---
+++ server/api/posts/post.ts
@@ -0,0 +1,20 @@
+import posts from '../../../data/post.json';
+import { H3Event } from 'h3';
+import { Http_Codes, Server_Error_Message } from '../../utils/constants';
+
+export default eventHandler(
+    async (event: H3Event): Promise<{    
+        data?: object | null;
+        count?: number;}> => {
+      try {    
+        setResponseStatus(event, Http_Codes.succuss);
+        return { data: posts, count: posts.length };
+        
+      } catch (err: any) {
+        return createError({ 
+          statusCode: err.statusCode || Http_Codes.serverError,
+          statusMessage: err.statusMessage || Server_Error_Message
+        });
+      }
+    }
+  );
---
+++ server/utils/constants.ts
@@ -0,0 +1,9 @@
+export const Prisma_Record_Does_Not_Exist_Code = "P2025";
+export const Server_Error_Message = "Algo salio mal, favor de esperar";
+export const Http_Codes = {
+  succuss: 200,
+  serverError: 500,
+  created: 201,
+  notFound: 404,
+  validationError: 422
+};
